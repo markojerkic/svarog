b6468e73c075fe5b77263b99c39e621ac4d3fd7a
Marko Jerkić <markojerkic266@gmail.com>
2024-04-20 13:32:58 +0200

    test input files

--- client/reader/reader_test.go
+++ client/reader/reader_test.go
@@ -5,45 +5,58 @@ import (
 	"testing"
 )
 
-func TestReader(t *testing.T) {
-	var reader Reader
-	output := make(chan *Line)
-	done := make(chan bool)
+func TestReaderImplementation(t *testing.T) {
+	var _ Reader = (*ReaderImpl)(nil) // Ensure ReaderImpl implements the Reader interface
+}
 
-	// Create a temp file and append lines to it
-	f, err := os.CreateTemp("", "test")
+func TestReaderRun(t *testing.T) {
+	input, err := os.CreateTemp("", "testfile.txt")
+	defer os.Remove(input.Name())
+
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.Remove(f.Name())
 
-	reader = NewReader(f, output)
-	for i := 0; i < 100; i++ {
-		f.WriteString("line\n")
+	input.WriteString("test\nline\n")
+	input.Close()
+
+	output := make(chan *Line, 10)
+
+	input, err = os.Open(input.Name())
+
+	if err != nil {
+		t.Fatalf("Failed to open file: %v", err)
 	}
-	go reader.Run(done)
 
-	for i := 0; i < 100; i++ {
-		f.WriteString("line\n")
-	}
-	f.Close()
+	r := NewReader(input, output)
+	stopSignal := make(chan bool)
 
-	numLines := 0
-	t.Log("numLines: ", numLines)
+	go r.Run(stopSignal)
+
+	expectedLines := []string{"test", "line"}
+
+	receivedLines := make([]string, 0, 2)
+
+	// Wait for Run to finish
+	<-stopSignal
 
 	for {
-		select {
-		case line := <-output:
-			if line == nil {
-				t.Log("line is nil")
-				break
-			}
-			numLines++
-		case <-done:
-			if numLines != 200 {
-				t.Errorf("expected 100 lines, got %d", numLines)
-			}
-			return
+		line := <-output
+		if line == nil {
+			break
+		}
+
+		receivedLines = append(receivedLines, line.LogLine)
+
+	}
+
+	if len(receivedLines) != len(expectedLines) {
+		t.Errorf("Expected %d lines, got %d", len(expectedLines), len(receivedLines))
+	}
+
+	for i, line := range expectedLines {
+		if receivedLines[i] != line {
+			t.Errorf("Expected line '%s', got '%s'", line, receivedLines[i])
 		}
 	}
 
