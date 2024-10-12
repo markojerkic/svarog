2e2ea617773b0cc4e8eefd238e53ae8d7b1eda81
Marko Jerkić <markojerkic266@gmail.com>
2024-10-12 19:21:49 +0200

    add one more test

--- tests/web-socket/instances_test.go
+++ tests/web-socket/instances_test.go
@@ -11,7 +11,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func TestAddInstance(t *testing.T) {
+func TestRemoveInstance(t *testing.T) {
 	markoSubscription := ws.LogsHub.Subscribe("marko")
 
 	firstId := primitive.NewObjectID()
@@ -88,5 +88,86 @@ func TestAddInstance(t *testing.T) {
 	assert.Equal(t, "::2", markoUpdates[1].Client.IpAddress)
 	assert.Equal(t, thirdId, markoUpdates[2].ID)
 	assert.Equal(t, "::1", markoUpdates[2].Client.IpAddress)
+
+}
+
+func TestAddInstance(t *testing.T) {
+	markoSubscription := ws.LogsHub.Subscribe("marko")
+	(*markoSubscription).RemoveInstance("::2")
+
+	firstId := primitive.NewObjectID()
+	ws.LogsHub.NotifyInsert(types.StoredLog{
+		ID:        firstId,
+		LogLine:   "marko",
+		Timestamp: time.Now(),
+		Client: types.StoredClient{
+			ClientId:  "marko",
+			IpAddress: "::1",
+		},
+		SequenceNumber: 0,
+	})
+
+	secondId := primitive.NewObjectID()
+	ws.LogsHub.NotifyInsert(types.StoredLog{
+		ID:        secondId,
+		LogLine:   "marko",
+		Timestamp: time.Now(),
+		Client: types.StoredClient{
+			ClientId:  "marko",
+			IpAddress: "::2",
+		},
+		SequenceNumber: 0,
+	})
+
+	(*markoSubscription).AddInstance("::2")
+	(*markoSubscription).RemoveInstance("::1")
+
+	thirdId := primitive.NewObjectID()
+	ws.LogsHub.NotifyInsert(types.StoredLog{
+		ID:        thirdId,
+		LogLine:   "marko",
+		Timestamp: time.Now(),
+		Client: types.StoredClient{
+			ClientId:  "marko",
+			IpAddress: "::1",
+		},
+		SequenceNumber: 0,
+	})
+
+	fourthId := primitive.NewObjectID()
+	ws.LogsHub.NotifyInsert(types.StoredLog{
+		ID:        fourthId,
+		LogLine:   "marko",
+		Timestamp: time.Now(),
+		Client: types.StoredClient{
+			ClientId:  "marko",
+			IpAddress: "::2",
+		},
+		SequenceNumber: 0,
+	})
+
+	markoUpdates := make([]types.StoredLog, 0, 10)
+	timeout, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	for {
+		isDone := false
+		select {
+		case log, ok := <-(*markoSubscription).GetUpdates():
+			isDone = !ok
+			markoUpdates = append(markoUpdates, log)
+		case <-timeout.Done():
+			isDone = true
+		}
+		if isDone {
+			break
+		}
+	}
+
+	assert.Equal(t, 2, len(markoUpdates))
+
+	assert.Equal(t, firstId, markoUpdates[0].ID)
+	assert.Equal(t, "::1", markoUpdates[0].Client.IpAddress)
+	assert.Equal(t, fourthId, markoUpdates[1].ID)
+	assert.Equal(t, "::2", markoUpdates[1].Client.IpAddress)
 
 }
