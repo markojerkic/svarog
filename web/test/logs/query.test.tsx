d0231be4914e9547b776c138918c5525cd4d8c24
Marko Jerkić <markojerkic266@gmail.com>
2024-10-16 22:37:01 +0200

    mess aroung with tests

--- web/test/logs/query.test.tsx
+++ web/test/logs/query.test.tsx
@@ -1,47 +1,71 @@
 import { renderHook, waitFor } from "@solidjs/testing-library";
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
-// import { http, HttpResponse } from "msw";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
 import { type ParentProps, createSignal } from "solid-js";
-import { describe, expect, it } from "vitest";
-import { createLoqQuery } from "../../src/lib/store/query";
+import { afterAll, afterEach, beforeAll, describe, expect, test } from "vitest";
+import { createLoqQuery } from "~/lib/store/query";
+import type { LogLine } from "~/lib/store/log-store";
 
-// const handlers = [
-//     http.get(`${import.meta.env.VITE_API_URL}/v1/logs/*`, () => {
-//         return HttpResponse.json({
-//             data: {
-//                 logs: [
-//                     { id: 1, message: "Hello" },
-//                     { id: 2, message: "World" },
-//                 ],
-//             },
-//         });
-//     }),
-// ];
+const mockData = [
+	{
+		id: "1",
+		content: "Hello",
+		timestamp: new Date().getTime(),
+		sequenceNumber: 1,
+		client: {
+			clientId: "marko",
+			ipAddress: "::1",
+		},
+	},
+] satisfies LogLine[];
+
+const handlers = [
+	http.get(`${import.meta.env.VITE_API_URL}/v1/logs/*`, () => {
+		console.log("Hello from mock");
+		return HttpResponse.json(mockData);
+	}),
+];
+const mockServer = setupServer(...handlers);
 
 describe("createLoqQuery", () => {
-    const queryClient = new QueryClient();
+	beforeAll(() => mockServer.listen());
+	afterEach(() => mockServer.resetHandlers());
+	afterAll(() => mockServer.close());
 
-    const Wrapper = (props: ParentProps) => (
-        <QueryClientProvider client={queryClient}>
-            {props.children}
-        </QueryClientProvider>
-    );
+	const queryClient = new QueryClient();
 
-    const [clientId, _setClientId] = createSignal("marko");
-    const [selectedInstances, _setSelectedInstances] = createSignal<
-        string[] | undefined
-    >(undefined);
-    const [search, _setSearch] = createSignal<string | undefined>(undefined);
+	const Wrapper = (props: ParentProps) => (
+		<QueryClientProvider client={queryClient}>
+			{props.children}
+		</QueryClientProvider>
+	);
 
-    it("should initialize and return data", async () => {
-        const { result } = renderHook(
-            () => createLoqQuery(clientId, selectedInstances, search),
-            {
-                wrapper: Wrapper,
-            },
-        );
+	const [clientId, _setClientId] = createSignal("marko");
+	const [selectedInstances, _setSelectedInstances] = createSignal<
+		string[] | undefined
+	>(undefined);
+	const [search, _setSearch] = createSignal<string | undefined>(undefined);
 
-        await waitFor(() => result.queryDetails.isSuccess);
-        expect(result.queryDetails.data).toEqual({});
-    });
+	test("should initialize and return data", async () => {
+		const { result } = renderHook(
+			() => createLoqQuery(clientId, selectedInstances, search),
+			{
+				wrapper: Wrapper,
+			},
+		);
+		await result.queryDetails.fetchNextPage();
+
+		await waitFor(() => {
+			return result.queryDetails.isSuccess;
+		});
+		expect(
+			result.queryDetails.isSuccess,
+			"Query needs to resolve to success",
+		).toBeTruthy();
+		expect(result.data.size, "Data should hold exactly one element").toEqual(1);
+		expect(result.data.get(0), "Query data needs to be as expected").toEqual(
+			mockData[0],
+		);
+	});
 });
