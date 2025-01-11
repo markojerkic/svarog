26e2175b3e211ea6800a57808a4e630e6ba43c80
Marko Jerkić <markojerkic266@gmail.com>
2025-01-11 01:14:07 +0100

    start refacroting

--- web/src/components/log-viewer.tsx
+++ web/src/components/log-viewer.tsx
@@ -1,5 +1,5 @@
-import { createVirtualizer } from "@tanstack/solid-virtual";
-import { For, createEffect, createMemo, onMount } from "solid-js";
+
+import { createEffect, createMemo, onMount } from "solid-js";
 import { useInstanceColor } from "@/lib/hooks/instance-color";
 import {
 	createLogQueryOptions,
@@ -14,25 +14,13 @@ import {
 } from "@tanstack/solid-query";
 import { createMachine } from "@solid-primitives/state-machine";
 import { newLogLineListener } from "@/lib/store/connection";
-import { useWindowHeight } from "@/lib/hooks/use-window-height";
-import { ScrollToBottomButton } from "./scroll-to-bottom-button";
+import { ScrollArea } from "./scroll-area";
 
 export const LogViewer = (props: {
 	clientId: string;
 	selectedInstances: string[];
 	searchQuery?: string;
 }) => {
-	// biome-ignore lint/style/useConst: Needs to be let for solidjs to be able to track it
-	let logsRef: HTMLDivElement | undefined = undefined;
-	// biome-ignore lint/style/useConst: Needs to be let for solidjs to be able to track it
-	let topRef: HTMLDivElement | undefined = undefined;
-	// biome-ignore lint/style/useConst: Needs to be let for solidjs to be able to track it
-	let bottomRef: HTMLDivElement | undefined = undefined;
-	const windowHeight = useWindowHeight();
-	const scrollViewerHeight = () => `${Math.ceil(windowHeight() * 0.8)}px`;
-
-	const isLockedInBottom = () => false;
-
 	// LOGS
 	const queryClient = useQueryClient();
 	const query = createInfiniteQuery(() => createLogQueryOptions(() => props));
@@ -45,30 +33,6 @@ export const LogViewer = (props: {
 	});
 
 	const logCount = () => logs().length;
-
-	const virtualizer = createVirtualizer({
-		get count() {
-			return logCount();
-		},
-		estimateSize: () => 25,
-		getScrollElement: () => logsRef ?? null,
-		overscan: 5,
-	});
-	const scrollToBottom = () => {
-		virtualizer.scrollToIndex(logs().length, { align: "end" });
-	};
-
-	const observer = new IntersectionObserver((entries) => {
-		for (const entry of entries) {
-			if (entry.isIntersecting) {
-				if (entry.target.id === "bottom" && !query.isFetchingNextPage) {
-					query.fetchNextPage();
-				} else if (entry.target.id === "top" && !query.isFetchingPreviousPage) {
-					query.fetchPreviousPage();
-				}
-			}
-		}
-	});
 
 	const queryState = createMachine<{
 		idle: { value: { isFetching: () => void } };
@@ -88,7 +52,8 @@ export const LogViewer = (props: {
 				const isDone = () => {
 					to("isDoneFetchingPreviousPage");
 					console.log("Scrolling to index", query.data?.pages[0].length);
-					virtualizer.scrollToIndex(query.data?.pages[0].length ?? 0);
+					//virtualizer.scrollToIndex(query.data?.pages[0].length ?? 0);
+					console.error("Treba scroll");
 				};
 
 				return { isDone };
@@ -117,15 +82,6 @@ export const LogViewer = (props: {
 	});
 
 	onMount(() => {
-		scrollToBottom();
-	});
-
-	onMount(() => {
-		observer.observe(topRef!);
-		observer.observe(bottomRef!);
-	});
-
-	onMount(() => {
 		const unsub = newLogLineListener((line) => {
 			const queryKey = createLogQueryOptions(() => props).queryKey;
 			queryClient.setQueryData(
@@ -139,64 +95,39 @@ export const LogViewer = (props: {
 		return () => unsub();
 	});
 
-	const items = virtualizer.getVirtualItems();
-
 	return (
-		<div
-			ref={logsRef}
-			class="scrollbar-thin scrollbar-track-zinc-900 scrollbar-thumb-zinc-700 ml-4 rounded-l-md border border-black"
-			style={{
-				height: scrollViewerHeight(),
-				width: "90vw%",
-				"overflow-y": "auto",
+		<ScrollArea
+			fetchPrevious={() => {
+				console.log("Daj previous");
 			}}
+			fetchNext={() => {
+				console.log("Daj next");
+			}}
+			itemCount={logCount()}
 		>
-			<div
-				style={{
-					height: `${virtualizer.getTotalSize()}px`,
-					width: "100%",
-					position: "relative",
-				}}
-			>
-				<ScrollToBottomButton
-					scrollToBottom={scrollToBottom}
-					isLockedInBottom={isLockedInBottom()}
-				/>
-				<div
-					style={{
-						position: "absolute",
-						top: 0,
-						left: 0,
-						width: "100%",
-						transform: `translateY(${items.length ? items[0].start : 0}px)`,
-					}}
-				>
-					<div id="top" ref={topRef} />
-					<For each={virtualizer.getVirtualItems()}>
-						{(virtualItem) => {
-							const item = logs()[virtualItem.index];
-							const color = useInstanceColor(item.client.ipAddress);
+			{(virtualItem) => {
+				const item = logs()[virtualItem.index];
+				const color = useInstanceColor(item.client.ipAddress);
 
-							return (
-								<pre
-									data-index={virtualItem.index}
-									class={"border-l-4 pl-2 text-black hover:border-l-8"}
-									style={{
-										"--tw-border-opacity": 1,
-										"border-left-color": color(),
-									}}
-									ref={(el) =>
-										queueMicrotask(() => virtualizer.measureElement(el))
-									}
-								>
-									{item.content}
-								</pre>
-							);
+				return (
+					<pre
+						data-index={virtualItem.index}
+						class={"border-l-4 pl-2 text-black hover:border-l-8"}
+						style={{
+							"--tw-border-opacity": 1,
+							"border-left-color": color(),
 						}}
-					</For>
-					<div id="bottom" class="my-[-2rem]" ref={bottomRef} />
-				</div>
-			</div>
-		</div>
+						ref={(_el) =>
+							queueMicrotask(() => {
+								//virtualizer.measureElement(el)
+								console.warn("Nezz triba li ovo popraviti");
+							})
+						}
+					>
+						{item.content}
+					</pre>
+				);
+			}}
+		</ScrollArea>
 	);
 };
---
+++ web/src/components/scroll-area.tsx
@@ -0,0 +1,92 @@
+import { useWindowHeight } from "@/lib/hooks/use-window-height";
+import { createViewportObserver } from "@solid-primitives/intersection-observer";
+import { type VirtualItem, createVirtualizer } from "@tanstack/solid-virtual";
+import { For, type JSXElement, createSignal, onMount } from "solid-js";
+import { ScrollToBottomButton } from "./scroll-to-bottom-button";
+
+type ScrollAreaProps = {
+	itemCount: number;
+	fetchNext: () => void;
+	fetchPrevious: () => void;
+	children: (virtualItem: VirtualItem) => JSXElement;
+};
+
+export const ScrollArea = (props: ScrollAreaProps) => {
+	// biome-ignore lint/style/useConst: Needs to be let for solidjs to be able to track it
+	let logsRef: HTMLDivElement | undefined = undefined;
+	const windowHeight = useWindowHeight();
+	const scrollViewerHeight = () => `${Math.ceil(windowHeight() * 0.8)}px`;
+	const [isLockedInBottom, _setIsLockedInBotton] = createSignal(false);
+
+	const virtualizer = createVirtualizer({
+		get count() {
+			return props.itemCount;
+		},
+		estimateSize: () => 25,
+		getScrollElement: () => logsRef ?? null,
+		overscan: 5,
+	});
+	const scrollToBottom = () => {
+		virtualizer.scrollToIndex(props.itemCount, { align: "end" });
+	};
+
+	const [intersectionObserver] = createViewportObserver();
+
+	onMount(() => {
+		scrollToBottom();
+	});
+	const items = virtualizer.getVirtualItems();
+
+	return (
+		<div
+			ref={logsRef}
+			class="scrollbar-thin scrollbar-track-zinc-900 scrollbar-thumb-zinc-700 ml-4 rounded-l-md border border-black"
+			style={{
+				height: scrollViewerHeight(),
+				width: "90vw%",
+				"overflow-y": "auto",
+			}}
+		>
+			<div
+				style={{
+					height: `${virtualizer.getTotalSize()}px`,
+					width: "100%",
+					position: "relative",
+				}}
+			>
+				<ScrollToBottomButton
+					scrollToBottom={scrollToBottom}
+					isLockedInBottom={isLockedInBottom()}
+				/>
+				<div
+					style={{
+						position: "absolute",
+						top: 0,
+						left: 0,
+						width: "100%",
+						transform: `translateY(${items.length ? items[0].start : 0}px)`,
+					}}
+				>
+					<div
+						id="top"
+						use:intersectionObserver={() => {
+							console.log("to top");
+							props.fetchPrevious();
+						}}
+					/>
+					<For each={virtualizer.getVirtualItems()}>
+						{(virtualItem) => props.children(virtualItem)}
+					</For>
+					<div
+						id="bottom"
+						class="my-[-2rem]"
+						use:intersectionObserver={() => {
+							console.log("to bottom");
+							props.fetchNext();
+						}}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
